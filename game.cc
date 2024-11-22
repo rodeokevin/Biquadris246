13c9278039faf77deaf8af1585750d5588aa56a3
bobetpopo <r436liu@uwaterloo.ca>
2024-11-22 15:31:16 -0500

    change game structure to accommodate player turn logic

--- game.cc
+++ game.cc
@@ -1,15 +1,14 @@
 #include "game.h"
+
 #include "board.h"
 
 // Get the state of one of the Boards
 char Game::getState(int board, int row, int col) const {
     if (board == 1) {
         return board1->charAt(row, col);
-    }
-    else {
+    } else {
         return board2->charAt(row, col);
     }
-    
 }
 
 int Game::getLevel(int p) {
@@ -20,15 +19,26 @@ int Game::getScore(int p) {
     return (p == 1) ? p1Score : p2Score;
 }
 
-Game::Game(int p1Level, int p2Level, Board *board1, Board *board2)
-    :p1Level{p1Level},p2Level{p2Level},board1{board1},board2{board2}{}
+int Game::getPlayerTurn() {
+    return currentPlayer;
+}
 
-void Subject::attach( Observer* o ) {
+void Game::switchPlayerTurn() {
+    currentPlayer = currentPlayer == 0 ? 1 : 0;
+}
+
+Game::Game(int p1Level, int p2Level, int currentPlayer, Board* board1, Board* board2)
+    : p1Level{p1Level}, p2Level{p2Level}, currentPlayer{currentPlayer} {
+    boards[0] = board1;
+    boards[1] = board2;
+}
+
+void Subject::attach(Observer* o) {
     // Add the observer pointer to the back of the vector
     observers.push_back(o);
 }
 
-void Subject::detach( Observer* o ) {
+void Subject::detach(Observer* o) {
     // Find the observer and erase it (it does nothing if not found)
     for (auto it = observers.begin(); it != observers.end(); ++it) {
         if (*it == o) {
--- textObserver.d
+++
@@ -1,2 +0,0 @@
-textObserver.o: textObserver.cc textObserver.h observer.h game.h board.h \
- tile.h block.h
